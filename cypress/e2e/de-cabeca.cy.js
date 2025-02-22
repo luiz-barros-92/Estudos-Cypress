56b4742ed91f093224ae5ec22338fd4ce9c81514
Luiz Barros <lck.barros@gmail.com>
2025-02-21 23:04:10 -0300

    Teste de preenchimento de formulário, clique e verificação de surgimento de resposta

---
+++ cypress/e2e/de-cabeca.cy.js
@@ -0,0 +1,15 @@
+describe('De Cabeça - Sua Calculadora Online', () => {
+  beforeEach(() => {
+    cy.visit('https://luiz-barros-92.github.io/de-cabeca/')
+  })
+
+  it('verificar o titulo', () => {    
+    cy.title().should('eq', 'De Cabeça - Sua Calculadora Online')
+  })
+
+  it('preenchimento de formulários de Fahrenheit para Celsius', () => {
+    cy.get('#fahrenInput').type('15')
+    cy.get('input[type="button"][onclick="FahrenPara()"]').click({force: true})
+    cy.get('#celsiusOutput').should('be.visible')
+  })
+})
